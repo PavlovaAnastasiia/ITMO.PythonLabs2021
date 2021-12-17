3d2bef8629c39c34d0d699e72cf3f13b05eb81b7
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2021-12-17 15:30:26 +0300

    .

---
+++ lab1/TestCommand.py
@@ -0,0 +1,13 @@
+
+import math
+help(math)
+
+#узнать набор методов объекта
+print(dir(math))
+
+#информация только об отдельном методе
+help(math.sqrt)
+
+list1 = [82, 8, 23, 97, 92]
+dir(list1)
+help(list1.insert)
---
+++ lab1/test.py
@@ -0,0 +1,16 @@
+
+print("Hello, World!")
+a = 5
+a = a + 3
+print('a = ', a)
+
+a = 5
+b = a + 7
+print('b = ', b)
+
+import time
+delay = 3
+a = int(input())
+print(a**2)
+time.sleep(delay)
+
